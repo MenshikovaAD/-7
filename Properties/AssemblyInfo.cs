b4fd0ecc50b24add1c690e97ee21a26c19d06dcf
Анастасия Меньшикова <menschickova2011@mail.ru>
2022-01-30 14:20:01 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Урок 7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Урок 7")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5ad44110-dfbc-4130-b7b9-3980685dce26")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Задача 7.1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Метод определяет площадь большего треугольника, площадь считается с помощью другого метода 
+            Console.WriteLine("Ввведите стороны треугольника N1");
+            Console.WriteLine("Сторона треугольника х1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сторона треугольника у1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сторона треугольника z1: ");
+            int z1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввведите стороны треугольника N2");
+            Console.WriteLine("Сторона треугольника х2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сторона треугольника у2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сторона треугольника z2: ");
+            int z2 = Convert.ToInt32(Console.ReadLine());
+            double S1, S2, P1, P2, S3, S4;
+            GetParam(x1, x2, y1, y2, z1, z2, out S1, out S2, out P1, out P2, out S3, out S4);
+                      
+            Console.ReadKey();
+        }
+        static void GetParam(int x1, int x2, int y1, int y2, int z1, int z2, out double S1, out double S2, out double P1, out double P2, out double S3, out double S4)
+        {
+            P1 =(x1+y1+z1);
+            P2 =(x2+y2+z2);
+            S1 = Math.Sqrt(P1/2*(P1/2-x1)*(P1/2-y1)*(P1/2-z1));
+            S2 = Math.Sqrt(P2 / 2 * (P2 / 2 - x2) * (P2 / 2 - y2) * (P2 / 2 - z2));
+            S3 = 0;
+            S4 = 0;
+            if (S1>S2)
+            {
+                S3 = S1;
+                S4 = S2;
+                Console.WriteLine("Площадь первого треугольника - {0}", Math.Round(S1, 2));
+                Console.WriteLine("Площадь второго треугольника - {0}", Math.Round(S2, 2));
+                Console.WriteLine("Площадь {0} > {1}", Math.Pow(S3, 2), Math.Round(S4, 2));
+            }
+            if (S2 > S1)
+            {
+                S3 = S2;
+                S4 = S1;
+                Console.WriteLine("Площадь первого треугольника - {0}", Math.Round(S1, 2));
+                Console.WriteLine("Площадь второго треугольника - {0}", Math.Round(S2, 2));
+                Console.WriteLine("Площадь {0} < {1}", Math.Pow(S3, 2), Math.Round(S4, 2));
+            }
+            else
+            {
+                S3 = S2;
+                S4 = S1;
+                Console.WriteLine("Площадь первого треугольника - {0}", Math.Round(S1, 2));
+                Console.WriteLine("Площадь второго треугольника - {0}", Math.Round(S2, 2));
+                Console.WriteLine("Площади равны {0} = {1}", Math.Round(S3, 2), Math.Round(S4, 2));
+            }
+        }
+    }
+}
